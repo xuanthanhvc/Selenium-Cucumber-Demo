12c0564917b99daf62a20c41c7c729489aa41580
Thanh Hoang <xuanthanhvc@gmail.com>
2024-06-28 03:11:08 +0700

    Add failsafe and screenshot after failure

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -2,6 +2,10 @@ package step_definitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import utilities.Driver;
 import utilities.DriverFactory;
 import utilities.PropertiesUtils;
@@ -17,8 +21,19 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            embedFinalScreenShot(scenario);
+        }
         Driver.quitDriver();
+    }
+
+    private void embedFinalScreenShot(Scenario scenario) {
+        if (Driver.getDriverInstance() != null) {
+            WebDriver driver = Driver.getDriverInstance();
+            final byte[] finalScreenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(finalScreenshot, "image/png", "[Screenshot][After Scenario] " + scenario.getName());
+        }
     }
 
 }
